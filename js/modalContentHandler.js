5e4e3f08a8605b964eb3e2b61630aa35c060e1de
mi-creative <james.al.leonard@gmail.com>
2020-05-11 17:39:09 +0200

    cleanup to modal window & code

---
+++ js/modalContentHandler.js
@@ -0,0 +1,259 @@
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+
+var projectName = document.getElementById("project-name");
+var projectGenre = document.getElementById("project-genre");
+var projectDescr = document.getElementById("project-descr");
+
+document.getElementById("barus").onclick = function(){
+	modalImg.src = this.src;
+
+	projectName.textContent = "Barus";
+	projectGenre.textContent = "Death Metal";
+	projectDescr.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id elit at libero ultrices tempus. Nam sit amet malesuada augue, vel interdum risus. Fusce volutpat ex diam, vitae bibendum ante placerat eu. Aenean tempor, turpis a sodales tristique, urna orci tincidunt velit, vel porttitor purus ipsum faucibus dolor. Vestibulum sollicitudin ultrices laoreet. Praesent quis viverra mauris. Curabitur nisi est, posuere vel tristique id, scelerisque sit amet elit.";
+
+	document.getElementById("mod-fb-li").style.display = "inline";
+	document.getElementById("mod-fb-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-bc-li").style.display = "inline";
+	document.getElementById("mod-bc-link").href = "rerere";
+
+	document.getElementById("mod-sc-li").style.display = "inline";
+	document.getElementById("mod-sc-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-yt-li").style.display = "inline";
+	document.getElementById("mod-yt-link").href = "rerere";
+
+	document.getElementById("mod-ins-li").style.display = "none";
+	document.getElementById("mod-ins-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-web-li").style.display = "none";
+	document.getElementById("mod-web-link").href = "rerere";
+
+}
+
+document.getElementById("maieutiste").onclick = function(){
+	modalImg.src = this.src;
+
+	projectName.textContent = "Maïeutiste";
+	projectGenre.textContent = "Progressive Black Metal";
+	projectDescr.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id elit at libero ultrices tempus. Nam sit amet malesuada augue, vel interdum risus. Fusce volutpat ex diam, vitae bibendum ante placerat eu. Aenean tempor, turpis a sodales tristique, urna orci tincidunt velit, vel porttitor purus ipsum faucibus dolor. Vestibulum sollicitudin ultrices laoreet. Praesent quis viverra mauris. Curabitur nisi est, posuere vel tristique id, scelerisque sit amet elit.";
+
+	document.getElementById("mod-fb-li").style.display = "inline";
+	document.getElementById("mod-fb-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-bc-li").style.display = "inline";
+	document.getElementById("mod-bc-link").href = "rerere";
+
+	document.getElementById("mod-sc-li").style.display = "inline";
+	document.getElementById("mod-sc-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-yt-li").style.display = "inline";
+	document.getElementById("mod-yt-link").href = "rerere";
+
+	document.getElementById("mod-ins-li").style.display = "none";
+	document.getElementById("mod-ins-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-web-li").style.display = "none";
+	document.getElementById("mod-web-link").href = "rerere";
+}
+
+document.getElementById("orcae").onclick = function(){
+	modalImg.src = this.src;
+
+	projectName.textContent = "Orcæ";
+	projectGenre.textContent = "Experimental Electroacoustic";
+	projectDescr.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id elit at libero ultrices tempus. Nam sit amet malesuada augue, vel interdum risus. Fusce volutpat ex diam, vitae bibendum ante placerat eu. Aenean tempor, turpis a sodales tristique, urna orci tincidunt velit, vel porttitor purus ipsum faucibus dolor. Vestibulum sollicitudin ultrices laoreet. Praesent quis viverra mauris. Curabitur nisi est, posuere vel tristique id, scelerisque sit amet elit.";
+
+	document.getElementById("mod-fb-li").style.display = "inline";
+	document.getElementById("mod-fb-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-bc-li").style.display = "inline";
+	document.getElementById("mod-bc-link").href = "rerere";
+
+	document.getElementById("mod-sc-li").style.display = "inline";
+	document.getElementById("mod-sc-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-yt-li").style.display = "inline";
+	document.getElementById("mod-yt-link").href = "rerere";
+
+	document.getElementById("mod-ins-li").style.display = "none";
+	document.getElementById("mod-ins-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-web-li").style.display = "none";
+	document.getElementById("mod-web-link").href = "rerere";
+}
+
+document.getElementById("epitaphe").onclick = function(){
+	modalImg.src = this.src;
+
+	projectName.textContent = "Epitaphe";
+	projectGenre.textContent = "Death / Doom Metal";
+	projectDescr.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id elit at libero ultrices tempus. Nam sit amet malesuada augue, vel interdum risus. Fusce volutpat ex diam, vitae bibendum ante placerat eu. Aenean tempor, turpis a sodales tristique, urna orci tincidunt velit, vel porttitor purus ipsum faucibus dolor. Vestibulum sollicitudin ultrices laoreet. Praesent quis viverra mauris. Curabitur nisi est, posuere vel tristique id, scelerisque sit amet elit.";
+
+	document.getElementById("mod-fb-li").style.display = "inline";
+	document.getElementById("mod-fb-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-bc-li").style.display = "inline";
+	document.getElementById("mod-bc-link").href = "rerere";
+
+	document.getElementById("mod-sc-li").style.display = "inline";
+	document.getElementById("mod-sc-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-yt-li").style.display = "inline";
+	document.getElementById("mod-yt-link").href = "rerere";
+
+	document.getElementById("mod-ins-li").style.display = "none";
+	document.getElementById("mod-ins-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-web-li").style.display = "none";
+	document.getElementById("mod-web-link").href = "rerere";
+}
+
+document.getElementById("gaillardon").onclick = function(){
+	modalImg.src = this.src;
+
+	projectName.textContent = "Nico Gaillardon";
+	projectGenre.textContent = "Multimedia & Noise/Drone Artist";
+	projectDescr.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id elit at libero ultrices tempus. Nam sit amet malesuada augue, vel interdum risus. Fusce volutpat ex diam, vitae bibendum ante placerat eu. Aenean tempor, turpis a sodales tristique, urna orci tincidunt velit, vel porttitor purus ipsum faucibus dolor. Vestibulum sollicitudin ultrices laoreet. Praesent quis viverra mauris. Curabitur nisi est, posuere vel tristique id, scelerisque sit amet elit.";
+
+	document.getElementById("mod-fb-li").style.display = "inline";
+	document.getElementById("mod-fb-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-bc-li").style.display = "inline";
+	document.getElementById("mod-bc-link").href = "rerere";
+
+	document.getElementById("mod-sc-li").style.display = "inline";
+	document.getElementById("mod-sc-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-yt-li").style.display = "inline";
+	document.getElementById("mod-yt-link").href = "rerere";
+
+	document.getElementById("mod-ins-li").style.display = "none";
+	document.getElementById("mod-ins-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-web-li").style.display = "none";
+	document.getElementById("mod-web-link").href = "rerere";
+}
+
+document.getElementById("liquidflesh").onclick = function(){
+	modalImg.src = this.src;
+
+	projectName.textContent = "Liquid Flesh";
+	projectGenre.textContent = "Purulent Death Metal";
+	projectDescr.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id elit at libero ultrices tempus. Nam sit amet malesuada augue, vel interdum risus. Fusce volutpat ex diam, vitae bibendum ante placerat eu. Aenean tempor, turpis a sodales tristique, urna orci tincidunt velit, vel porttitor purus ipsum faucibus dolor. Vestibulum sollicitudin ultrices laoreet. Praesent quis viverra mauris. Curabitur nisi est, posuere vel tristique id, scelerisque sit amet elit.";
+
+	document.getElementById("mod-fb-li").style.display = "inline";
+	document.getElementById("mod-fb-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-bc-li").style.display = "inline";
+	document.getElementById("mod-bc-link").href = "rerere";
+
+	document.getElementById("mod-sc-li").style.display = "inline";
+	document.getElementById("mod-sc-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-yt-li").style.display = "inline";
+	document.getElementById("mod-yt-link").href = "rerere";
+
+	document.getElementById("mod-ins-li").style.display = "none";
+	document.getElementById("mod-ins-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-web-li").style.display = "none";
+	document.getElementById("mod-web-link").href = "rerere";
+}
+
+document.getElementById("james").onclick = function(){
+	modalImg.src = this.src;
+
+	projectName.textContent = "Plastic Lobster";
+	projectGenre.textContent = "Sound Engineer, Digital Artist";
+	projectDescr.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id elit at libero ultrices tempus. Nam sit amet malesuada augue, vel interdum risus. Fusce volutpat ex diam, vitae bibendum ante placerat eu. Aenean tempor, turpis a sodales tristique, urna orci tincidunt velit, vel porttitor purus ipsum faucibus dolor. Vestibulum sollicitudin ultrices laoreet. Praesent quis viverra mauris. Curabitur nisi est, posuere vel tristique id, scelerisque sit amet elit.";
+
+	document.getElementById("mod-fb-li").style.display = "inline";
+	document.getElementById("mod-fb-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-bc-li").style.display = "inline";
+	document.getElementById("mod-bc-link").href = "rerere";
+
+	document.getElementById("mod-sc-li").style.display = "inline";
+	document.getElementById("mod-sc-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-yt-li").style.display = "inline";
+	document.getElementById("mod-yt-link").href = "rerere";
+
+	document.getElementById("mod-ins-li").style.display = "none";
+	document.getElementById("mod-ins-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-web-li").style.display = "none";
+	document.getElementById("mod-web-link").href = "rerere";
+}
+
+document.getElementById("demenseed").onclick = function(){
+	modalImg.src = this.src;
+
+	projectName.textContent = "Demenseed";
+	projectGenre.textContent = "Progressive Death Metal";
+	projectDescr.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id elit at libero ultrices tempus. Nam sit amet malesuada augue, vel interdum risus. Fusce volutpat ex diam, vitae bibendum ante placerat eu. Aenean tempor, turpis a sodales tristique, urna orci tincidunt velit, vel porttitor purus ipsum faucibus dolor. Vestibulum sollicitudin ultrices laoreet. Praesent quis viverra mauris. Curabitur nisi est, posuere vel tristique id, scelerisque sit amet elit.";
+
+	document.getElementById("mod-fb-li").style.display = "inline";
+	document.getElementById("mod-fb-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-bc-li").style.display = "inline";
+	document.getElementById("mod-bc-link").href = "rerere";
+
+	document.getElementById("mod-sc-li").style.display = "inline";
+	document.getElementById("mod-sc-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-yt-li").style.display = "inline";
+	document.getElementById("mod-yt-link").href = "rerere";
+
+	document.getElementById("mod-ins-li").style.display = "none";
+	document.getElementById("mod-ins-link").href = "https://www.facebook.com";
+
+	document.getElementById("mod-web-li").style.display = "none";
+	document.getElementById("mod-web-link").href = "rerere";
+}
+
+modalImg.onclick = function() {
+	modalfadeOut();
+}
+
+function modalfadeOut(){
+	$("#myModal").fadeOut();
+	document.getElementById("carousel").style.display = "block";
+}
+
+function modalfadeIn(){
+	$("#myModal").fadeIn();
+	document.getElementById("carousel").style.display = "none";
+}
+
+function hasClass(element, cls) {
+	return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+$(document).click(function(event) {
+	$target = $(event.target);
+	if(!hasClass($(event.target)[0], 'artist-image')){
+		if(!$target.closest('#proj-info').length)
+			if($("#myModal").is(":visible") === true)
+				if($('#proj-content').is(":visible"))
+					modalfadeOut();
+	}
+	else
+		modalfadeIn();
+});
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	modalfadeOut();
+}
