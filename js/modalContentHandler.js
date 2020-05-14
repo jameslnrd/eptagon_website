3afac89c5e24a6f7cadee1b202ad065b4ef7da54
mi-creative <james.al.leonard@gmail.com>
2020-05-14 20:17:36 +0200

    better gallery organisation & band content

--- js/modalContentHandler.js
+++ js/modalContentHandler.js
@@ -9,31 +9,30 @@ var modalImg = document.getElementById("img01");
 var projectName = document.getElementById("project-name");
 var projectGenre = document.getElementById("project-genre");
 var projectDescr = document.getElementById("project-descr");
+var projCopy = document.getElementById("proj-copyright");
 
 document.getElementById("barus").onclick = function(){
 	modalImg.src = this.src;
+	projCopy.innerHTML = "&copy;photo: Yog-Sothoth Photography";
 
-	projectName.textContent = "Barus";
+
+	projectName.textContent = "Barús";
 	projectGenre.textContent = "Death Metal";
-	projectDescr.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id elit at libero ultrices tempus. Nam sit amet malesuada augue, vel interdum risus. Fusce volutpat ex diam, vitae bibendum ante placerat eu. Aenean tempor, turpis a sodales tristique, urna orci tincidunt velit, vel porttitor purus ipsum faucibus dolor. Vestibulum sollicitudin ultrices laoreet. Praesent quis viverra mauris. Curabitur nisi est, posuere vel tristique id, scelerisque sit amet elit.";
+
+	projectDescr.innerHTML = 
+"The term βαρύς evokes a weight, an overbearing burden. <br/> \
+This takes musical form through a complex and utterly atmospherical, yet visceral, approach to Death Metal. A monolithic, churning sound merged with progressive structures, frequently delving into Black or Doom territories, drawing on the crystalline coldness of ambient and electroacoustic soundscapes. <br/> \
+The band, described by reviewers as <i>\"Autopsy reinterpreted by Meshuggah\"</i> or <i>\"Gojira and Gorguts jamming out to an old Demilich tune with Ulcerate's instruments\"</i> released a first EP through Emanations (a division of Les Acteurs de l’Ombre Productions) in 2015, following in 2018 with their first album, Drowned (through Memento Mori Productions). "
 
 	document.getElementById("mod-fb-li").style.display = "inline";
-	document.getElementById("mod-fb-link").href = "https://www.facebook.com";
-
+	document.getElementById("mod-fb-link").href = "https://www.facebook.com/barusband/";
 	document.getElementById("mod-bc-li").style.display = "inline";
-	document.getElementById("mod-bc-link").href = "rerere";
-
-	document.getElementById("mod-sc-li").style.display = "inline";
-	document.getElementById("mod-sc-link").href = "https://www.facebook.com";
-
+	document.getElementById("mod-bc-link").href = "https://barus.bandcamp.com/";
+	document.getElementById("mod-sp-li").style.display = "inline";
+	document.getElementById("mod-sp-link").href = "https://open.spotify.com/artist/6xwSb06WvA9gQxVUarw3wR";
 	document.getElementById("mod-yt-li").style.display = "inline";
-	document.getElementById("mod-yt-link").href = "rerere";
-
-	document.getElementById("mod-ins-li").style.display = "none";
-	document.getElementById("mod-ins-link").href = "https://www.facebook.com";
-
-	document.getElementById("mod-web-li").style.display = "none";
-	document.getElementById("mod-web-link").href = "rerere";
+	document.getElementById("mod-yt-link").href = "https://www.youtube.com/channel/UCMM_1N812Kf92cyP2exQCGQ";
+	
 
 }
 
@@ -94,25 +93,20 @@ document.getElementById("epitaphe").onclick = function(){
 
 	projectName.textContent = "Epitaphe";
 	projectGenre.textContent = "Death / Doom Metal";
-	projectDescr.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id elit at libero ultrices tempus. Nam sit amet malesuada augue, vel interdum risus. Fusce volutpat ex diam, vitae bibendum ante placerat eu. Aenean tempor, turpis a sodales tristique, urna orci tincidunt velit, vel porttitor purus ipsum faucibus dolor. Vestibulum sollicitudin ultrices laoreet. Praesent quis viverra mauris. Curabitur nisi est, posuere vel tristique id, scelerisque sit amet elit.";
+	projectDescr.innerHTML = "<b>Epitaphe</b> is a cavernous and psychedelic death metal band from the French Alps. <br/> \
+Influenced by bands such as Dead Congregation, Portal and Esoteric, Epitaphe create music that engulfs, with a powerful focus on overwhelming and opaque atmospheres. <br/> \
+Following the first demo, which received wide acclaim and was subsequently released on cd via Chaos Records), Epitaphe made a deal with Aesthetic Death to release their debut album - <b>I</b>. <br/> \
+I, in its doomed torment, is a distraught journey through blackness, fury and melancholia - using illustrations by the Finnish painter Petri Ala-Maunus. It was mixed, recorded and mastered by James Leonard of Plastic Lobster Studios, and released at the end of summer 2019. ";
 
 	document.getElementById("mod-fb-li").style.display = "inline";
-	document.getElementById("mod-fb-link").href = "https://www.facebook.com";
-
+	document.getElementById("mod-fb-link").href = "https://www.facebook.com/epitaphemetal/";
 	document.getElementById("mod-bc-li").style.display = "inline";
-	document.getElementById("mod-bc-link").href = "rerere";
+	document.getElementById("mod-bc-link").href = "https://epitaphe.bandcamp.com/";
+	document.getElementById("mod-sp-li").style.display = "inline";
+	document.getElementById("mod-sp-link").href = "https://open.spotify.com/artist/7Gy7eYT4baOOw66qWC7bc7";
+	document.getElementById("mod-tw-li").style.display = "inline";
+	document.getElementById("mod-tw-link").href = "https://twitter.com/EpitapheBand";
 
-	document.getElementById("mod-sc-li").style.display = "inline";
-	document.getElementById("mod-sc-link").href = "https://www.facebook.com";
-
-	document.getElementById("mod-yt-li").style.display = "inline";
-	document.getElementById("mod-yt-link").href = "rerere";
-
-	document.getElementById("mod-ins-li").style.display = "none";
-	document.getElementById("mod-ins-link").href = "https://www.facebook.com";
-
-	document.getElementById("mod-web-li").style.display = "none";
-	document.getElementById("mod-web-link").href = "rerere";
 }
 
 document.getElementById("gaillardon").onclick = function(){
@@ -198,43 +192,62 @@ document.getElementById("demenseed").onclick = function(){
 
 	projectName.textContent = "Demenseed";
 	projectGenre.textContent = "Progressive Death Metal";
-	projectDescr.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id elit at libero ultrices tempus. Nam sit amet malesuada augue, vel interdum risus. Fusce volutpat ex diam, vitae bibendum ante placerat eu. Aenean tempor, turpis a sodales tristique, urna orci tincidunt velit, vel porttitor purus ipsum faucibus dolor. Vestibulum sollicitudin ultrices laoreet. Praesent quis viverra mauris. Curabitur nisi est, posuere vel tristique id, scelerisque sit amet elit.";
+	projectDescr.innerHTML = 
+"Founded in 2010, <b>Demenseed</b> is a death metal band from the french Alps, drawing on a wide range of influences, from extreme metal to progressive rock, jazz and classical music. <br/>\
+In 2010, the band records a first demo entitled “Light Window” and performs many local shows in the following years. \
+In 2015, the band records a second opus, much more accomplished: the EP “Granite Forest”.\
+The album lasts about 48 minutes and the music is now clearly death metal oriented, including some progressive rock elements, especially on the last 17-minutes track. <br/>\
+In 2018, after some member replacements, the band releases 3 different EPs: “Blood Stained Lamb”, a new version of “Granite Forest” with new drum tracks and artwork, and finally the 3-tracks EP “Growing Darkness”, performed by the current line-up.";
 
 	document.getElementById("mod-fb-li").style.display = "inline";
-	document.getElementById("mod-fb-link").href = "https://www.facebook.com";
-
+	document.getElementById("mod-fb-link").href = "https://www.facebook.com/Demenseed";
 	document.getElementById("mod-bc-li").style.display = "inline";
-	document.getElementById("mod-bc-link").href = "rerere";
-
+	document.getElementById("mod-bc-link").href = "https://demenseed.bandcamp.com/";
 	document.getElementById("mod-sc-li").style.display = "inline";
-	document.getElementById("mod-sc-link").href = "https://www.facebook.com";
-
+	document.getElementById("mod-sc-link").href = "https://soundcloud.com/demenseed";
 	document.getElementById("mod-yt-li").style.display = "inline";
-	document.getElementById("mod-yt-link").href = "rerere";
+	document.getElementById("mod-yt-link").href = "https://www.youtube.com/channel/UCsJ2ISw4NpFe43GMQZZFaHg";
+	document.getElementById("mod-tw-li").style.display = "inline";
+	document.getElementById("mod-tw-link").href = "https://twitter.com/Demenseed_DM";
+	document.getElementById("mod-ins-li").style.display = "inline";
+	document.getElementById("mod-ins-link").href = "https://www.instagram.com/demenseed_dm/";
 
-	document.getElementById("mod-ins-li").style.display = "none";
-	document.getElementById("mod-ins-link").href = "https://www.facebook.com";
+}
 
-	document.getElementById("mod-web-li").style.display = "none";
-	document.getElementById("mod-web-link").href = "rerere";
+document.getElementById("fia").onclick = function(){
+	modalImg.src = this.src;
+
+	projectName.textContent = "Faith In Agony";
+	projectGenre.textContent = "Grunge / Alternative Metal";
+	projectDescr.innerHTML = "Straight out of a 90s garage, the emerging alternative metal band <b>Faith in Agony</b> distinguishes itself through vivid melodies, efficient riffs and persistent vocal harmonies. \r\nThe quartet proceeds frantically while mixing heavy dynamics with deep nuances, which reflects an electric energy created on stage. Riffing from grunge to blues, the musical ensemble has positioned itself within a dynamic and accessible musical style with a strong wish for proximity with its listeners. \r\nFaiths in Agony released their first eponymous EP in October 2016 and are presently defending their most recent opus <b>Do Not Repeat</b>. After standing out as the opening act for Ultra Vomit and during several festivals (Rock O'marais, Festirock), the band is actively working on the release of their first album.";
+
+	document.getElementById("mod-fb-li").style.display = "inline";
+	document.getElementById("mod-fb-link").href = "https://www.facebook.com/faithinagony/";
+	document.getElementById("mod-bc-li").style.display = "inline";
+	document.getElementById("mod-bc-link").href = "https://faithinagony.bandcamp.com/";
+	document.getElementById("mod-ins-li").style.display = "inline";
+	document.getElementById("mod-ins-link").href = "https://www.instagram.com/faith_in_agony/?hl=fr";
+
 }
 
 modalImg.onclick = function() {
 	modalfadeOut();
 }
-
 document.getElementById("project-descr").onclick = function() {
 	modalfadeOut();
 }
 
 function modalfadeOut(){
 	$("#myModal").fadeOut();
-	document.getElementById("carousel").style.display = "block";
+
+	for (let element of document.getElementsByClassName("mod-link")){
+		element.style.display="none";
+	 }
+	projCopy.innerHTML='';
 }
 
 function modalfadeIn(){
 	$("#myModal").fadeIn();
-	document.getElementById("carousel").style.display = "none";
 }
 
 function hasClass(element, cls) {
